aa47054a10dd22fa31f31c251c5393dc6caa4de2
MaxvanderWeide <m.t.vanderweide@gmail.com>
2022-11-14 23:53:15 +0100

    Added env.js

---
+++ front-end/src/env.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+    window.__env.apiUrl = "http://localhost:8080";
+
+    window.__env.apiToken = "token"
+
+    window.__env.enableDebug = true;
+}(this));
